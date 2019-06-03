e0c5e76780c442da02c5d7c94ac88b1ff05d1217
vitorroxo <vitor@mobi2buy.com>
2019-06-03 09:19:39 -0300

    Remove git cache

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -32,6 +32,15 @@ defmodule Rumbl.Accounts do
     |> Repo.insert()
   end
 
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
 
   alias Rumbl.Accounts.Credential
 
