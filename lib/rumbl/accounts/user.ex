31cc6ba303d72aa543ea5afe871661f33f2096d4
vitorroxo <vitor@mobi2buy.com>
2019-06-05 17:27:38 -0300

    Chapter-6 Generators and Relationships

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -18,12 +18,6 @@ defmodule Rumbl.Accounts.User do
     |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
   end
 
-  def registration_changeset(user, params) do
-    user
-    |> changeset(params)
-    |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
-  end
-
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:name, :username])
